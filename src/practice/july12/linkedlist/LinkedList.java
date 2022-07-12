8133958f8ea05c6881f957a012e0bf984ae2d892
parasharparth <parthparashar6@gmail.com>
2022-07-12 10:40:11 -0700

    July12-LinkedList-deleting elements done

--- src/practice/july12/linkedlist/LinkedList.java
+++ src/practice/july12/linkedlist/LinkedList.java
@@ -35,6 +35,56 @@ public class LinkedList {
         System.out.println("List after adding an element to a position: ");
         list.addToPosition(second,6);
         list.traversal();
+        System.out.println("Deleting elements from the head");
+        list.deleteFromHead();
+        list.traversal();
+        System.out.println("List after deleting from position: ");
+        list.deleteFromPosition(second);
+        list.traversal();
+        System.out.println("List after deleting from tail");
+        list.deleteFromTail();
+        list.traversal();
+    }
+
+    public void deleteFromTail()
+    {
+        if(head == null)
+        {
+            System.err.println("List is already empty");
+            return;
+        }
+        Node prev_node = head;
+        Node last = head.next;
+        while(last.next!= null)
+        {
+            last = last.next;
+            prev_node = prev_node.next;
+        }
+        prev_node.next = null;
+    }
+    public void deleteFromPosition(Node prev_node)
+    {
+        if(head == null)
+        {
+            System.err.println("The given list is already empty");
+            return;
+        }
+        if(prev_node == null)
+        {
+            System.err.println("Previous node null");
+            return;
+        }
+        prev_node.next = prev_node.next.next;
+    }
+
+    public void deleteFromHead()
+    {
+        if(head == null)
+        {
+            System.err.println("The list is already empty");
+            return;
+        }
+        head = head.next;
     }
 
     public void addToPosition(Node prev_node, int new_data)
