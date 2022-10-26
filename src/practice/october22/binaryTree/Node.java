f81d5cbce8fd2d62edba0d8ca59462f4501d77a6
parasharparth <parthparashar6@gmail.com>
2022-10-26 14:45:14 -0700

    October-26-Node

---
+++ src/practice/october22/binaryTree/Node.java
@@ -0,0 +1,13 @@
+package practice.october22.binaryTree;
+
+public class Node {
+
+    int key;
+    Node left, right;
+
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
