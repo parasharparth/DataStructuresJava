97450f5e923303ecb2b9a1214b03679fa2a17b9a
parasharparth <parthparashar6@gmail.com>
2022-10-27 23:58:03 -0700

    October-27-Binary-Tree

---
+++ src/practice/october27/binarytree/BinaryTree.java
@@ -0,0 +1,16 @@
+package practice.october27.binarytree;
+
+public class BinaryTree {
+
+    Node root;
+
+    BinaryTree()
+    {
+        root = null;
+    }
+
+    BinaryTree(int key)
+    {
+        root = new Node(key);
+    }
+}
