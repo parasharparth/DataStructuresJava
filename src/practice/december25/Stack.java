202ba0d7598c454a3772220ce12892e3f8ee8cfa
parasharparth <parthparashar6@gmail.com>
2022-12-25 23:03:15 -0800

    december-25-stack

---
+++ src/practice/december25/Stack.java
@@ -0,0 +1,88 @@
+package practice.december25;
+
+public class Stack {
+
+    int[] arr;
+    int capacity=5;
+    int top;
+
+    Stack()
+    {
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    boolean isEmpty()
+    {
+        if(top == -1)
+        {
+            System.out.println("Stack is Empty");
+            return true;
+        }
+        return false;
+    }
+
+    boolean isFull()
+    {
+        if(top == capacity-1)
+        {
+            System.out.println("Stack is Full");
+            return true;
+        }
+        return false;
+    }
+
+    void push(int element)
+    {
+        if(isFull())
+        {
+            return;
+        }
+        top = top + 1;
+        arr[top] = element;
+        System.out.println(element+" added");
+        return;
+    }
+
+    void pop()
+    {
+        if(isEmpty())
+        {
+            return;
+        }
+        int to_be_removed = arr[top];
+        top = top - 1;
+        System.out.println(to_be_removed +" removed");
+        return;
+    }
+
+    void traversal()
+    {
+        if(isEmpty())
+        {
+            return;
+        }
+        for(int i=top; i>=0; i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args)
+    {
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.traversal();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.traversal();
+    }
+}
