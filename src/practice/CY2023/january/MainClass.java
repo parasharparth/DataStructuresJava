0b6aee8e0ad9591cb91b0f3d4ca5415b6a449b17
parasharparth <parthparashar6@gmail.com>
2023-01-14 02:21:56 -0800

    Jan14-stack-main-class

---
+++ src/practice/CY2023/january/MainClass.java
@@ -0,0 +1,25 @@
+package practice.CY2023.january;
+
+import practice.CY2023.january.jan14.Stack;
+
+public class MainClass {
+
+    public static void main(String[] args)
+    {
+        System.out.println("-------------STACK OPERATIONS--------------");
+        Stack stack = new Stack();
+        System.out.println("-----------PUSH OPERATION---------------");
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println("--------------STACK AFTER PUSH OPERATION-------------");
+        stack.traversal();
+        System.out.println("-----------POP OPERATION------------");
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println("--------------STACK AFTER POP OPERATION-------------");
+        stack.traversal();
+    }
+}
