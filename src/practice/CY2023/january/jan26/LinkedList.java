e83a26a6e6d6c642815ec7342823aacedff33b95
parasharparth <parthparashar6@gmail.com>
2023-01-26 21:58:49 -0800

    January-26-Linked-List

---
+++ src/practice/CY2023/january/jan26/LinkedList.java
@@ -0,0 +1,89 @@
+package practice.CY2023.january.jan26;
+
+public class LinkedList {
+
+    public static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    Node head;
+
+    public static void main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(4);
+        list.head.next = second;
+        second.next = third;
+        third.next = null;
+        list.traversal();
+        list.addToHead(0);
+        list.traversal();
+        list.addToTail(5);
+        list.traversal();
+        list.addToPosition(second,3);
+        list.traversal();
+    }
+
+    public void traversal()
+    {
+        Node n = head;
+        while(n!=null)
+        {
+            System.out.print(n.data+" ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public void addToHead(int new_data)
+    {
+        if(head == null)
+        {
+            head = new Node(new_data);
+            head.next = null;
+            return;
+        }
+        Node new_Node = new Node(new_data);
+        new_Node.next = head;
+        head = new_Node;
+    }
+
+    public void addToTail(int new_data)
+    {
+        if(head == null)
+        {
+            head = new Node(new_data);
+            head.next = null;
+            return;
+        }
+        Node new_Node = new Node(new_data);
+        Node n = head;
+        while(n.next!=null)
+        {
+            n = n.next;
+        }
+        n.next = new_Node;
+        new_Node.next = null;
+    }
+
+    public void addToPosition(Node prev_Node, int new_data)
+    {
+        if(prev_Node == null)
+        {
+            System.out.println("Previous Node cannot be null");
+            return;
+        }
+        Node new_node = new Node(new_data);
+        new_node.next = prev_Node.next;
+        prev_Node.next = new_node;
+    }
+}
