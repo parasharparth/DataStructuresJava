33babe62e30ac550c39fac6bd8056c70d0416853
parasharparth <parthparashar6@gmail.com>
2023-01-26 21:58:29 -0800

    January-26-Stack

---
+++ src/practice/CY2023/january/jan26/Stack.java
@@ -0,0 +1,66 @@
+package practice.CY2023.january.jan26;
+
+public class Stack {
+
+    int[] arr;
+    int top;
+    int capacity = 5;
+
+    public Stack()
+    {
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    public boolean isEmpty()
+    {
+        if(top == -1)
+        {
+            System.out.println("The stack is empty");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull()
+    {
+        if(top == capacity -1)
+        {
+            System.out.println("Stack is Full");
+            return true;
+        }
+        return false;
+    }
+
+    public void push(int element)
+    {
+        if(isFull())
+        {
+            return;
+        }
+        top = top + 1;
+        arr[top] = element;
+        System.out.println(element+" added");
+    }
+
+    public void pop()
+    {
+        if(isEmpty())
+        {
+            return;
+        }
+        int removed = arr[top];
+        top = top - 1;
+        System.out.println(removed+" removed");
+    }
+
+    public void traversal()
+    {
+        for(int i=top; i>=0; i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+}
