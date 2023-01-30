c7bc1aa32f77651711a39b90d43126fd98bdf80a
parasharparth <parthparashar6@gmail.com>
2023-01-29 22:30:10 -0800

    January-28-Stack

---
+++ src/practice/CY2023/january/jan28/Stack.java
@@ -0,0 +1,19 @@
+package practice.CY2023.january.jan28;
+
+public class Stack {
+
+    int[] arr;
+    int top;
+    int capacity = 5;
+
+    public Stack()
+    {
+        arr = new int[capacity];
+        top = -1;
+    }
+
+    public boolean isEmpty()
+    {
+        return true;
+    }
+}
