4cfde6569aeeff733696890516d12ba8597ac8d7
parasharparth <parthparashar6@gmail.com>
2023-02-03 16:34:29 -0800

    Linked-List

---
+++ src/practice/CY2023/february/feb03/LinkedList.java
@@ -0,0 +1,22 @@
+package practice.CY2023.february.feb03;
+
+public class LinkedList {
+	public static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data = data;
+			next = null;
+		}
+	}
+	Node head;
+	public static void main(String[] args){
+		LinkedList list = new LinkedList();
+		list.head = new Node(1);
+		Node second = new Node(2);
+		Node third = new Node(3);
+		list.head.next = second;
+		second.next = third;
+		third.next = null;
+	}
+}
